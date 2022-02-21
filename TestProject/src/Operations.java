7dca14a1bbc443f60290c98983be199e90ec7fa4
shatpaki <shatpaki@host.docker.internal>
2022-02-21 23:30:07 +0530

    Added class Operations.java

---
+++ TestProject/src/Operations.java
@@ -0,0 +1,12 @@
+public class Operations 
+{
+	
+	public static void main(String args[])
+	{
+		System.out.println("Hello2");
+		int a=10,b=10;
+		int c = a+b;
+		System.out.println("The Addition of a and b is : "+c);	
+	}
+
+}
