4aefca712b06c801478d541ee3597b5f63e015c2
shatpaki <100147476+shatpaki@users.noreply.github.com>
2022-02-22 12:48:11 +0530

    Update Operations.java
    updated sysout stmt

--- TestProject/src/Operations.java
+++ TestProject/src/Operations.java
@@ -4,7 +4,7 @@ public class Operations
 	public static void main(String args[])
 	{
 
-		System.out.println("Hello!");
+		System.out.println("Hello from Github!");
 		int a=10,b=10;
 		int c = a*b;
 		System.out.println("The multiplication of a and b is : "+c);	
