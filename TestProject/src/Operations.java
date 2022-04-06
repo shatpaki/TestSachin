c1c084380ca43f5c8db3d3476adcfad33869e0a6
SachinH <sachin.hatpaki@gmail.com>
2022-04-06 16:58:09 +0530

    Test update Operations.java

--- TestProject/src/Operations.java
+++ TestProject/src/Operations.java
@@ -4,7 +4,7 @@ public class Operations
 	//C:\WORKSPACES\Sample WS
 	public static void main(String args[])
 	{
-		System.out.println("Hello from Eclipse!");
+		System.out.println("Hello from Eclipse! from Eclipse");
 		int a=10,b=10;
 		int c = a*b;
 		System.out.println("The multiplication of a and b is : "+c);	
